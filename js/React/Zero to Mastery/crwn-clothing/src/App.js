6feceb22bd3a73f59dde236671a345ec8dc13a5f
web-development <dward1711@gmail.com>
2020-06-15 23:25:16 -0500

    adding header component, links to shome, shop, and contact pages

--- js/React/Zero to Mastery/crwn-clothing/src/App.js
+++ js/React/Zero to Mastery/crwn-clothing/src/App.js
@@ -5,12 +5,16 @@ import CollectionPreview from './components/collection-preview/collection-previe
 import './App.css';
 import './pages/homepage/homepage.styles.scss'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component.jsx';
+
 
 function App() {
   return (
     <div>
       {/* Use BrowserRouter, Switch, and Route to navigate the pages */}
       <BrowserRouter>
+        { /* Place the Header outside of the Switch */}
+        <Header />
         <Switch>
           <Route exact path='/' component={ HomePage } />
           <Route path='/shop' component={ ShopPage } />
